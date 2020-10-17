eb5cc16028adb2dfca4916fc4c0a58a62c08101d
Hayyim <b2banalytica@gmail.com>
2020-10-17 23:36:48 +0300

    hw2_2

--- HW2_2/ViewController.swift
+++ HW2_2/ViewController.swift
@@ -10,11 +10,79 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var coloreView: UIView!
+    
+    @IBOutlet weak var redNameLable: UILabel!
+    @IBOutlet weak var greenNameLable: UILabel!
+    @IBOutlet weak var blueNameLable: UILabel!
+    
+    @IBOutlet weak var redInfluenceLable: UILabel!
+    @IBOutlet weak var greenInfluenceLable: UILabel!
+    @IBOutlet weak var blueInfluenceLable: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    var currentRedValue: CGFloat = 0
+    var currentGreenValue: CGFloat = 0
+    var currentBlueValue: CGFloat = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        currentRedValue = CGFloat(redSlider.value)
+        currentGreenValue = CGFloat(greenSlider.value)
+        currentBlueValue = CGFloat(blueSlider.value)
+        
+        coloreView.backgroundColor = UIColor(red: currentRedValue, green: currentGreenValue, blue: currentBlueValue, alpha: 1)
+        
+        coloreView.layer.cornerRadius = 20
+        coloreView.layer.borderWidth = 3
+        coloreView.layer.borderColor = CGColor(srgbRed: 0, green: 0, blue: 0, alpha: 1)
+        
+        redNameLable.text = "Red:"
+        greenNameLable.text = "Green:"
+        blueNameLable.text = "Blue:"
+        
+        redNameLable.textAlignment = .right
+        greenNameLable.textAlignment = .right
+        blueNameLable.textAlignment = .right
+        
+        redInfluenceLable.text = String(redSlider.value)
+        greenInfluenceLable.text = String(greenSlider.value)
+        blueInfluenceLable.text = String(blueSlider.value)
+        
+        redInfluenceLable.textAlignment = .left
+        greenInfluenceLable.textAlignment = .left
+        blueInfluenceLable.textAlignment = .left
+     
+        redSlider.minimumTrackTintColor = .systemRed
+        greenSlider.minimumTrackTintColor = .systemGreen
+        blueSlider.minimumTrackTintColor = .systemBlue
+        
     }
 
 
+    @IBAction func addRedBySlider() {
+        currentRedValue = CGFloat(redSlider.value)
+        redInfluenceLable.text = String(format: "%1.2f", redSlider.value)
+        coloreView.backgroundColor = UIColor(red: currentRedValue, green: currentGreenValue, blue: currentBlueValue, alpha: 1)
+    }
+    
+    
+    @IBAction func addGreenSlider() {
+        currentGreenValue = CGFloat(greenSlider.value)
+        greenInfluenceLable.text = String(format: "%1.2f", greenSlider.value)
+        coloreView.backgroundColor = UIColor(red: currentRedValue, green: currentGreenValue, blue: currentBlueValue, alpha: 1)
+    }
+    
+    
+    @IBAction func addBlueSlider() {
+        currentBlueValue = CGFloat(blueSlider.value)
+        blueInfluenceLable.text = String(format: "%1.2f", blueSlider.value)
+        coloreView.backgroundColor = UIColor(red: currentRedValue, green: currentGreenValue, blue: currentBlueValue, alpha: 1)
+    }
+    
 }
 
